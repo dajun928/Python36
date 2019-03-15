63921a90c2564e22202fbb989be3ed99eaec0d67
dajun928 <1663177102@qq.com>
2019-03-15 23:45:36 +0800

    update

--- itcast/requests_test.py
+++
@@ -1,7 +0,0 @@
-#encoding=utf-8
-import requests
-
-
-response = requests.get("http://www.baidu.com/")
-
-print(response.content)
--- itcast/test02.py
+++
@@ -1,8 +0,0 @@
-#encoding=utf-8
-import random
-offices = [[1],[2],[3]]
-# index = random.randint(0,2)
-i=0
-while i<=50:
-    print(offices[random.randint(0,2)])
-    i=i+1

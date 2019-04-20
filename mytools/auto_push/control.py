e25bf3ce4065e3a73ddb8f6716908bc944a301ab
dajun928 <1663177102@qq.com>
2019-04-20 10:57:08 +0800

    Update control.py

--- mytools/auto_push/control.py
+++ mytools/auto_push/control.py
@@ -13,22 +13,14 @@ import subprocess
 import os
 import shutil
 
-
-#通过校验MD5 判断B内的文件与A 不同
-def get_MD5(file_path):
-    files_md5 = os.popen('md5 %s' % file_path).read().strip()
-    file_md5 = files_md5.replace('MD5 (%s) = ' % file_path, '')
-    return file_md5
-
 def copydir(path, out):
     for files in os.listdir(path):
         name = os.path.join(path, files)
         back_name = os.path.join(out, files)
         if os.path.isfile(name):
             if os.path.isfile(back_name):
-                if get_MD5(name) != get_MD5(back_name):
-                    pass
-                    # print(back_name)
+                # print(back_name)
+                pass
             else:
                 # print(back_name)
                 pass
@@ -36,8 +28,6 @@ def copydir(path, out):
             if not os.path.isdir(back_name):
                 os.makedirs(back_name)
                 copydir(name, back_name)
-
-
 
 def copyfile(src, dsc):
     tmp_list=[]
@@ -54,16 +44,16 @@ def copyfile(src, dsc):
 
 def cut_single_file():
     A = r"/root/homework"
-    B = r"/root/Projects/gitdemo/test/Tool"
+    B = r"/root/Projects/gitdemo/test/Tool/Tool"
+
+    # A=r"D:\test\a"
+    # B=r"D:\test\b"
     copydir(A, B)
     result = copyfile(A, B)
-    # print(result)
     if result:
         src_file, dsc_path = result.pop()
-        # print(src_file)
-        # print(dsc_path)
+        print(src_file)
         shutil.move(src_file, dsc_path)
-
 
 #本类的方法
 def push():
@@ -75,12 +65,13 @@ if __name__ == '__main__':
     scheduler = BlockingScheduler()
 
     # 设置定时调度本类的方法
-    scheduler.add_job(cut_single_file, 'cron', hour ='16',minute ='20')
+    scheduler.add_job(cut_single_file, 'cron', hour ='23',minute ='22')
 
-    scheduler.add_job(push, 'cron', hour ='16',minute ='20')
+    scheduler.add_job(push, 'cron', hour ='23',minute ='23')
 
     # 启动调度
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
+    # cut_single_file()

b3ea12726364bff6c8c8b48a78e7152b53c67829
dajun928 <1663177102@qq.com>
2019-05-07 23:51:53 +0800

    Create work.py

---
+++ work.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : work.py
+@time : 2019/05/07 23:39:57
+@func : 
+"""
+'''
+
+腾讯翻译接口封装
+
+https://console.cloud.tencent.com/tmt/manage
+
+https://github.com/TencentCloud/tencentcloud-sdk-python
+'''

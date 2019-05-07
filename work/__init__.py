9d2634ffbc7aa05db70a43ba0fcd9b5a9856418c
dajun928 <1663177102@qq.com>
2019-05-07 23:53:56 +0800

    add

---
+++ work/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : __init__.py.py
+@time : 2019/05/07 23:52:40
+@func : 
+"""
+import platform
+
+print(platform.python_version())
---
+++ work/test.py
@@ -0,0 +1,25 @@
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.tmt.v20180321 import tmt_client, models
+
+try:
+    cred = credential.Credential("AKIDShgQukxejY7QTVDyMUN0OWP7ZooYbLMA", "mKdhX8UaQ5UdXBaqrdJn34qOimbQ0MTL")
+    httpProfile = HttpProfile()
+    httpProfile.endpoint = "tmt.tencentcloudapi.com"
+
+
+    clientProfile = ClientProfile()
+    clientProfile.httpProfile = httpProfile
+    client = tmt_client.TmtClient(cred, "ap-guangzhou", clientProfile)
+
+    req = models.TextTranslateRequest()
+    params = '{"SourceText":"hello world","Source":"en","Target":"zh","ProjectId":10177715}'
+    req.from_json_string(params)
+
+    resp = client.TextTranslate(req)
+    print(resp.to_json_string())
+
+except TencentCloudSDKException as err:
+    print(err)

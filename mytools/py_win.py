7a2f896a512b89c597bd310bbf4ca0f8262b0ee0
dajun928 <1663177102@qq.com>
2019-06-12 01:07:50 +0800

    add

--- mytools/py_win.py
+++ mytools/py_win.py
@@ -4,7 +4,7 @@
 @version : 3.6.3
 @file : win_control.py
 @time : 2019/03/16 16:16:23
-@func : 连接本地windows 并执行操作命令
+@func : 连接本地windows或者linux 并执行操作命令
 """
 import subprocess
 
@@ -12,6 +12,7 @@ cmd = 'ls'
 a = subprocess.getstatusoutput(cmd)
 
 print(type(a))
+print(a[0])
 print(a[1])
 
 

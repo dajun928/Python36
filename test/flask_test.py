70e41dd5b6346ec3dc0f754241217351bfa15ac0
dajun928 <1663177102@qq.com>
2019-06-18 23:32:49 +0800

    Create flask_test.py

---
+++ test/flask_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : flask_test.py
+@time : 2019/06/16 23:10:59
+@func : 
+"""
+import platform
+
+print(platform.python_version())

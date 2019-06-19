838705006398ed501c56fb0753d1559349d4cc92
dajun928 <1663177102@qq.com>
2019-06-19 23:50:22 +0800

    add

--- flask_test_demo/test.py
+++ flask_test_demo/test.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-  
 """
 @version : 
-@file : flask_test.py
-@time : 2019/06/16 23:10:59
+@file : test.py
+@time : 2019/06/19 23:35:08
 @func : 
 """
 #导入Flask扩展
-from flask import Flask
+from flask import Flask,render_template
 
 # 创建Flask应用实例
 app = Flask(__name__)
@@ -15,7 +15,8 @@ app = Flask(__name__)
 # 定义路由及视图函数
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # return 'Hello World!'
+    return render_template('index.html')
 
 # 启动程序
 if __name__ == '__main__':

9cc1c41fc4b34ffd3613f4ccbf46f6c3fcebd789
dajun928 <1663177102@qq.com>
2019-07-13 23:25:18 +0800

    redis docker test
    redis docker test

---
+++ redis_test/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : __init__.py.py
+@time : 2019/07/13 23:14:22
+@func : 
+"""
+import platform
+
+print(platform.python_version())

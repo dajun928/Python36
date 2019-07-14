ceedec3bd7620ea46c8b920df06aa4d7701f4675
dajun928 <1663177102@qq.com>
2019-07-15 07:25:06 +0800

    1

---
+++ docker_test/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : __init__.py.py
+@time : 2019/07/14 20:10:46
+@func : 
+"""
+import platform
+
+print(platform.python_version())
---
+++ docker_test/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : test.py
+@time : 2019/07/14 20:10:57
+@func : https://www.cnblogs.com/sammy1989/p/9406899.html
+"""
+import platform
+
+print(platform.python_version())

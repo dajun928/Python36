342cd3e72e929502963af544fda855340b11b630
dajun928 <1663177102@qq.com>
2021-01-08 16:54:09 +0800

    用xlwt作数据分析  备份

--- tools/processing_data.py
+++ tools/processing_data.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-  
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
 """
-@version : 
-@file : processing_data.py
-@time : 2021/01/07 16:41:12
-@func : 
+@version : 2.7.14
+@file : processingdata.py
+@time : 2021/01/08 23:20:36
+@func : 对数据进行统计，并用xlwt包写入excel(生产环境安装pandas依赖的包太多，环境变更麻烦)
 """
 import datetime
 import xlwt
-import pdb
 
-def write_excel(file_path, data1,data2):
+def write_excel(file_path, data1, data2):
     f = xlwt.Workbook(encoding='utf-8')
+    # 写入sheet1
     sheet1 = f.add_sheet(u"统计数据", cell_overwrite_ok=True)
     title1 = ["管理员", "总计设备数", "设备类型", "分类设备数", "netbase监控率", "Wup监控率"]
-    # data1.insert(0, title1)
     style = xlwt.XFStyle()
     align = xlwt.Alignment()
     align.horz = 1
@@ -22,38 +21,32 @@ def write_excel(file_path, data1,data2):
     for j in range(len(title1)):
         sheet1.write(0, j, title1[j], style=style)
     i = 1
-
-    items_num=len(data1)
-    tmp1_set=()
+    dict_list = []
+    tmp_dict = {}
     for data in data1:
-        items_num-=1
-        device_kinds=data[-1]
-        data=data[:-1]
-        tmp_set=(data[0],data[1],device_kinds)
-        print(tmp_set)
-        for a in range(device_kinds):
-            print(a)
-        print(11111111111111111111111111111111111)
-        # print(device_kinds)
+        device_kinds = data[-1]
+        data = data[:-1]
+        keys_list = [list(m.keys())[0] for m in dict_list]
+        if data[0] not in keys_list:
+            tmp_dict[data[0]] = (data[1], device_kinds)
+            dict_list.append(tmp_dict)
+            tmp_dict = {}
         for j in range(len(data)):
-            sheet1.write(i, j, data[j], style=style)
-            # print(device_kinds)
-        i+=1
-    set1=("张三",7,4)
-    sheet1.write_merge(1,set1[2],0,0, set1[0])  # 合并列单元格
-    sheet1.write_merge(1,set1[2],1,1,set1[1])  # 合并列单元格
-    set2=('李四', 5, 3)
-    sheet1.write_merge(set1[2]+1,set1[2]+set2[2], 0, 0, u'暂无2')  # 合并列单元格
-    sheet1.write_merge(set1[2]+1,set1[2]+set2[2], 1, 1, 3)  # 合并列单元格
-    set3=('王五', 5, 3)
-    sheet1.write_merge(8, 10, 0, 0, u'暂无3')  # 合并列单元格
-    sheet1.write_merge(8, 10, 1, 1, 3)  # 合并列单元格
-
+            sheet1.write(i, j, data[j], style=style)  # 往单元格写入数据
+        i += 1
+    step0 = 0
+    for k in dict_list:
+        name = list(k.keys())[0]
+        number, step = list(k.values())[0]
+        sheet1.write_merge(step0 + 1, step0 + step, 0, 0, name)  # 合并单元格
+        sheet1.write_merge(step0 + 1, step0 + step, 1, 1, number)
+        step0 += step
     f.save(file_path)
 
+    # 写入sheet2
     sheet2 = f.add_sheet(u"设备详细信息", cell_overwrite_ok=True)
     title1 = ["管理IP", "监控类型", "主机名", "设备管理员", "netbase监控", "Wup监控"]
-    data2.insert(0, title1)  # 写入表头
+    data2.insert(0, title1)
     style = xlwt.XFStyle()
     align = xlwt.Alignment()
     align.horz = 1
@@ -63,8 +56,7 @@ def write_excel(file_path, data1,data2):
         for j in range(len(data)):
             sheet2.write(i, j, data[j], style=style)
         i = i + 1
-    # f.save(file_path)
-
+    f.save(file_path)
 
 
 def analysis_data(datas):
@@ -83,45 +75,46 @@ def analysis_data(datas):
                 device_type.setdefault(i[1], []).append(i)
             else:
                 device_type[i[1]] = [i]
-        davice_counts=len(device_type)
+        davice_counts = len(device_type)
         device_type_keys = device_type.keys()
-        # pdb.set_trace()
         for device_type_key in device_type_keys:
             items = device_type[device_type_key]
             items_num = len(items)
             netbase_true_num = 0
             whatsup_true_num = 0
+            # todo 有可能存在bug
+            # True if item[-2].lower() == "true" else False
             for item in items:
                 if item[-2]:
                     netbase_true_num += 1
                 if item[-1]:
                     whatsup_true_num += 1
-            netbse_rate = '{:.2f}%'.format(netbase_true_num / items_num * 100)
-            wup_rate = '{:.2f}%'.format(whatsup_true_num / items_num * 100)
-            tmp_list.append([k, sum_devices, device_type_key, items_num, netbse_rate, wup_rate,davice_counts])
+            netbse_rate = '{:.2f}%'.format(float(netbase_true_num) / float(items_num) * 100)
+            wup_rate = '{:.2f}%'.format(float(whatsup_true_num) / float(items_num) * 100)
+            tmp_list.append([k, sum_devices, device_type_key, items_num, netbse_rate, wup_rate, davice_counts])
     return tmp_list
 
+
 if __name__ == '__main__':
-    datas = [['192.168.178.1', '安全客户端', '192.168.178.1', '张三 ', False, True],
-             ['192.168.178.2', '安全客户端', '192.168.178.2', '张三 ', True, True],
-             ['192.168.178.3', '安全客户端', '发斯蒂芬斯蒂芬', '张三 ', False, True],
-             ['192.168.178.4', '防火墙', '192.168.178.4', '张三 ', True, True],
+    datas = [['192.168.178.1', '安全客户端', '192.168.178.1', '张三', False, True],
+             ['192.168.178.2', '安全客户端', '192.168.178.2', '张三', True, True],
+             ['192.168.178.3', '安全客户端', '发斯蒂芬斯蒂芬', '张三', False, True],
+             ['192.168.178.4', '防火墙', '192.168.178.4', '张三', True, True],
              ['192.168.178.5', '防火墙', '192.168.178.5', '李四', True, True],
              ['192.168.178.6', '防火墙', '法法', '李四', False, True],
              ['192.168.178.7', '防火墙', '192.168.178.7', '李四', True, True],
              ['192.168.178.8', '交换机', '撒阿发发', '王五', False, True],
-             ['192.168.178.9', '交换机', '192.168.178.9', '张三 ', True, True],
+             ['192.168.178.9', '交换机', '192.168.178.9', '张三', True, True],
              ['192.168.178.10', '异常行为', '192.168.178.10', '王五', True, True],
-             ['192.168.178.11', '异常行为', '按时发发发', '张三 ', False, True],
+             ['192.168.178.11', '异常行为', '按时发发发', '张三', False, True],
              ['192.168.178.12', '异常行为', '192.168.178.12', '王五', True, True],
-             ['192.168.178.13', '异常行为', '啊啊是发发', '张三 ', True, True],
-             ['192.168.178.14', '安全客户端', '192.168.178.14', '李四', True, True],
+             ['192.168.178.13', '异常行为', '啊啊是发发', '张三', True, True],
+             ['192.168.178.14', '安全客户端', '192.168.178.14', '张三', True, True],
              ['192.168.178.15', '交换机', '192.168.178.15', '王五', False, True],
              ['192.168.178.16', '交换机', '啊发发发生发发', '李四', True, True],
              ['192.168.178.17', '安全客户端', '192.168.178.17', '王五', False, True]]
-    tmp_list=analysis_data(datas)
-    # print(tmp_list)
+    # 统计数据
+    tmp_list = analysis_data(datas)
+    # 写入excel
     str_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    write_excel("安全管理员对应设备类型统计%s.xls" % str_time,tmp_list,datas)
-
-    
+    write_excel(u"安全管理员对应设备类型统计%s.xls" % str_time, tmp_list, datas)
